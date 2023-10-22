134df8a04e2ef82947ab636aa9f366752e05a334
SimonSubrini <simonsubrini@gmail.com>
2023-10-21 21:13:16 -0300

    Implemente las funciones para modificar el ancho de la línea y empece a trabajar en la GUI

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import cv2
 import mediapipe as mp
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from threading import Thread
+from PIL import Image, ImageTk
+import numpy as np
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-drawn_points = []
-touch_threshold = 40  # Dimensiones del "borrador"
-
-# Inicialización de diccionario para rastrear los dedos
+# Set constants
 fingers = {
     'thumb': [],
     'index': [],
@@ -17,13 +16,23 @@ fingers = {
     'pinky': []
 }
 
-# Colores
 colors = [
     (255, 0, 0), (255, 0, 255), (0, 255, 0), (0, 255, 255)
 ]
 
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+width, height = 860, 640
 
-# Función para obtener las coordenadas de los dedos
+# Declaration of variables
+drawn_points = []
+touch_threshold = 40
+line_width = 10
+frame = None
+
+
+# Declaration of functions
 def getFingerPosition(landmarks):
     fingers['thumb'] = [landmarks[1], landmarks[2], landmarks[3], landmarks[4]]
     fingers['index'] = [landmarks[6], landmarks[7], landmarks[8]]
@@ -33,66 +42,127 @@ def getFingerPosition(landmarks):
     return fingers
 
 
-# Función para detectar el toque y eliminar puntos
-def detectAndEraseTouches(hand_landmarks, points):
-    thumb = fingers['thumb']
-    thumb_tip = hand_landmarks[4]  # Punto de la punta del pulgar
-    mean_thumb = (thumb[0].x + thumb[1].x + thumb[2].x + thumb[2].x)/4
-
-    if (thumb[0].x + thumb[3].x)/2 > mean_thumb > (thumb[1].x + thumb[2].x)/2:
+def detectAndEraseTouches(finger_landmarks, points):
+    thumb_tip = fingers['thumb'][3]
+    thumb = finger_landmarks['thumb']
+    index = finger_landmarks['index']
+    middle = finger_landmarks['middle']
+    ring = finger_landmarks['ring']
+    pinky = finger_landmarks['pinky']
+    thumb_up = thumb[3].y < index[2].y and thumb[3].y < middle[2].y and thumb[3].y < ring[2].y and thumb[3].y < pinky[
+        2].y
+    if thumb_up:
         for point in points:
-            x, y, _ = point
+            x, y, _, _ = point
             distance = ((thumb_tip.x * width - x) ** 2 + (thumb_tip.y * height - y) ** 2) ** 0.5
             if distance < touch_threshold:
                 points.remove(point)
+        return True
+    return False
 
 
-# Función para agregar puntos en función de otros dedos
 def addPointsForFingers(finger_landmarks):
     for c, finger in enumerate(finger_landmarks):
         if finger[0].y > finger[1].y > finger[2].y:
             x, y = int(finger[2].x * width), int(finger[2].y * height)
-            drawn_points.append((x, y, colors[c]))
+            drawn_points.append((x, y, colors[c], line_width))
 
 
-with mp_hands.Hands(
-        static_image_mode=False,
-        max_num_hands=1,
-        min_detection_confidence=0.5) as hands:
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+def save_image():
+    file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+    if file_path:
+        cv2.imwrite(file_path, frame)
+        messagebox.showinfo("Guardado", "La imagen se ha guardado exitosamente.")
 
-        frame = cv2.resize(frame, (860, 640))
-        frame = cv2.flip(frame, 1)
-        height, width, _ = frame.shape
 
+def change_line_width(val):
+    global line_width
+    line_width = int(val)
+
+
+def change_eraser_width(val):
+    global touch_threshold
+    touch_threshold = int(val)
+
+
+def update_image():
+    global frame
+    if frame is not None:
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(frame_rgb)
+        photo = ImageTk.PhotoImage(image=Image.fromarray(frame_rgb))
+        label.config(image=photo)
+        label.image = photo
+        label.after(10, update_image)
 
-        if results.multi_hand_landmarks is not None:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                    mp_drawing.DrawingSpec(color=(143, 146, 219), thickness=2, circle_radius=4),
-                    mp_drawing.DrawingSpec(color=(179, 208, 65), thickness=4))
 
-                fingers = getFingerPosition(hand_landmarks.landmark)
+def process_images():
+    ret = True
+    with mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=1,
+            min_detection_confidence=0.5) as hands:
+        while ret:
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-                detectAndEraseTouches(hand_landmarks.landmark, drawn_points)
+            frame = cv2.resize(frame, (width, height))
+            frame = cv2.flip(frame, 1)
 
-                addPointsForFingers([fingers['index'], fingers['middle'], fingers['ring'], fingers['pinky']])
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(frame_rgb)
 
-        # Dibujar todos los puntos almacenados
-        for point in drawn_points:
-            x, y, color = point
-            cv2.circle(frame, (x, y), 10, color, -1)
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                        mp_drawing.DrawingSpec(color=(143, 146, 219), thickness=2, circle_radius=4),
+                        mp_drawing.DrawingSpec(color=(179, 208, 65), thickness=4))
 
-        cv2.imshow("Frame", frame)
+                    fingers = getFingerPosition(hand_landmarks.landmark)
 
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
+                    if not detectAndEraseTouches(fingers, drawn_points):
+                        addPointsForFingers([fingers['index'], fingers['middle'], fingers['ring'], fingers['pinky']])
 
-cap.release()
-cv2.destroyAllWindows()
+            for point in drawn_points:
+                x, y, color, size = point
+                cv2.circle(frame, (x, y), size, color, -1)
+
+            cv2.imshow("Frame", frame)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    root.quit()  # Detener la GUI al finalizar el procesamiento de imágenes
+
+
+# GUI elements
+root = tk.Tk()
+root.title("drawingWithHands")
+
+save_button = tk.Button(root, text="Guardar Imagen", command=save_image)
+save_button.pack()
+
+line_width_slider = tk.Scale(root, from_=5, to=25, orient="horizontal", label="Ancho de Línea",
+                             command=change_line_width)
+line_width_slider.set(10)
+line_width_slider.pack()
+
+eraser_width_slider = tk.Scale(root, from_=10, to=20, orient="horizontal", label="Ancho del Borrador",
+                               command=change_eraser_width)
+eraser_width_slider.set(20)
+eraser_width_slider.pack()
+
+label = tk.Label(root)
+label.pack()
+
+update_image()
+image_thread = Thread(target=process_images)
+image_thread.daemon = True
+image_thread.start()
+
+root.geometry("900x700")
+root.protocol("WM_DELETE_WINDOW", root.quit)  # Manejar el cierre de ventana
+root.mainloop()
